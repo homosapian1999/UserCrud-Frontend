fd5395c6390f5e1ac69a2fad0a6278b86e03bc92
homosapian <ankitd349@gmail.com>
2025-02-01 14:05:28 +0530

    layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,17 @@
-import Link from "next/link";
 import Providers from "@/components/Providers";
 import "../app/globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "User Crud App",
+  description: "This is my awesome CRUD App!",
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
         <Providers>
-          {/* <nav className="bg-gray-800 text-white p-4 flex gap-4">
-            <Link href="/" className="hover:text-gray-300">
-              Home
-            </Link>
-            <Link href="/users" className="hover:text-gray-300">
-              Users
-            </Link>
-          </nav> */}
           <main className="p-4">{children}</main>
         </Providers>
       </body>
